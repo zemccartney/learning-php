3cfcd2980306cbc063e0784af3aae0fb1f3f2e0d
Zack McCartney <zemccartney@gmail.com>
2017-03-12 22:16:58 -0400

    Work through W3 articles

--- phpman/useful.php
+++ phpman/useful.php
@@ -13,4 +13,18 @@
   }
 ?>
 
-<?php echo strpos($_SERVER['HTTP_USER_AGENT'], 'Chrome'); ?>
+<?php #echo strpos($_SERVER['HTTP_USER_AGENT'], 'Chrome'); ?>
+
+<?php
+// V2 OF USERAGENT FUNCTION ABOVE
+
+if (strpos($_SERVER['HTTP_USER_AGENT'], 'Chrome') !== FALSE) {
+    // get position of string
+    $position = strpos($_SERVER['HTTP_USER_AGENT'], 'Chrome');
+    // use position to pull out that word from word count associative array
+    echo strtoupper(str_word_count($_SERVER['HTTP_USER_AGENT'], 2)[$position]) . '<br />';
+    echo 'You are using Google Chrome.<br />';
+  } else {
+    echo 'GET postconed';
+  }
+?>
